01812e3d804c4bfaafeb2e5185d41fba5821ac6a
Diana <dianabistrik@gamil.com>
2026-01-18 20:34:58 +0200

    Add project files.

---
+++ Telhai.DotNet.PlayerProject/MusicPlayer.xaml.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Telhai.DotNet.PlayerProject.Models;
+
+namespace Telhai.DotNet.PlayerProject
+{
+    /// <summary>
+    /// Interaction logic for MusicPlayer.xaml
+    /// </summary>
+    public partial class MusicPlayer : Window
+    {
+        // Global Variables
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private DispatcherTimer timer = new DispatcherTimer();
+        private List<MusicTrack> library = new List<MusicTrack>();
+        private bool isDragging = false;
+        private const string FILE_NAME = "library.json";
+
+        public string MyProperty { get; set; } = "xxx";
+        public MusicPlayer()
+        {
+            // Init all Hardcoded xaml into Element Tree
+            InitializeComponent();
+
+            // 1. Setup Timer
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += new EventHandler(Timer_Tick);
+
+            // LoadLibrary();
+            this.Loaded += MusicPlayer_Loaded; // do it as event
+
+            //this.MouseDoubleClick += MusicPlayer_MouseDoubleClick;
+            // this.MouseDoubleClick += new MouseButtonEventHandler(MusicPlayer_MouseDoubleClick); - the same
+
+        }
+
+        private void MusicPlayer_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.LoadLibrary();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // Update slider ONLY if music is loaded AND user is NOT holding the handle
+            if (mediaPlayer.Source != null && mediaPlayer.NaturalDuration.HasTimeSpan && !isDragging)
+            {
+                sliderProgress.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliderProgress.Value = mediaPlayer.Position.TotalSeconds;
+            }
+        }
+
+
+        //private void MusicPlayer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //{
+        //    //MessageBox.Show("double click");
+        //    MainWindow p = new MainWindow();
+        //    p.Title = "yyy";
+        //    p.Show();
+        //}
+
+        // --- EMPTY PLACEHOLDERS TO MAKE IT BUILD ---
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                btn.Background = Brushes.LightGreen;
+            }
+
+            mediaPlayer.Play();
+            timer.Start();
+            txtStatus.Text = "Playing";
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+            txtStatus.Text = "Paused";
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+            timer.Stop();
+            sliderProgress.Value = 0;
+            txtStatus.Text = "Stopped";
+        }
+
+        private void SliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.Volume = sliderVolume.Value;
+        }
+
+
+        private void Slider_DragStarted(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true; // Stop timer updates
+        }
+
+        private void Slider_DragCompleted(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false; // Resume timer updates
+            mediaPlayer.Position = TimeSpan.FromSeconds(sliderProgress.Value);
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // File Dialog to choose file from system
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;     // choose multiple files at once
+            ofd.Filter = "MP3 Files|*.mp3";
+
+            // User Confirmed
+            if (ofd.ShowDialog() == true)
+            {
+                // itirate all files selected as string
+                foreach (string file in ofd.FileNames)  // the string if the file path
+                {
+                    // create object for each file
+                    MusicTrack track = new MusicTrack
+                    {
+                        // only file name
+                        Title = System.IO.Path.GetFileNameWithoutExtension(file),
+                        // full path
+                        FilePath = file
+                    };
+                    library.Add(track);
+                }
+                UpdateLibraryUI();
+                SaveLibrary();
+            }
+        }
+
+        // Helper method to refresh the box
+        private void UpdateLibraryUI()
+        {
+            // Take all library list as source to the listbox
+            // display tostring for inner object within list
+            lstLibrary.ItemsSource = null;
+            lstLibrary.ItemsSource = library;
+        }
+
+        private void SaveLibrary()
+        {
+            var options = new JsonSerializerOptions {  WriteIndented = true };
+            string json = JsonSerializer.Serialize(library, options);
+            File.WriteAllText(FILE_NAME, json);
+        }
+
+        private void LoadLibrary()
+        {
+            if (File.Exists(FILE_NAME))
+            {
+                // read file
+                string json = File.ReadAllText(FILE_NAME);
+                //create list of MusicTrack from json
+                library = JsonSerializer.Deserialize<List<MusicTrack>>(json) ?? new List<MusicTrack>();
+                // show all loaded Musictrak in listbox
+                UpdateLibraryUI();
+            }
+        }
+
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstLibrary.SelectedItem is MusicTrack track)
+            {
+                library.Remove(track);
+                UpdateLibraryUI();
+                SaveLibrary();
+            }
+        }
+
+        private void LstLibrary_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lstLibrary.SelectedItem is MusicTrack track)
+            {
+                mediaPlayer.Open(new Uri(track.FilePath));
+                mediaPlayer.Play();
+                timer.Start();
+                txtCurrentSong.Text = track.Title;
+                txtStatus.Text = "Playing";
+            }
+        }
+
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settingsWin = new Settings();
+
+            // Listen for the results
+            settingsWin.OnScanCompleted += SettingsWin_OnScanCompleted;
+
+            settingsWin.ShowDialog();
+        }
+
+        private void SettingsWin_OnScanCompleted(List<MusicTrack> newTracksEventData)
+        {
+            foreach (var track in newTracksEventData)
+            {
+                // Prevent duplicates based on FilePath
+                if (!library.Any(x => x.FilePath == track.FilePath))
+                {
+                    library.Add(track);
+                }
+            }
+
+            UpdateLibraryUI();
+            SaveLibrary();
+        }
+
+    }
+}
---
+++ Telhai.DotNet.PlayerProject/Services/ItunesService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Telhai.DotNet.PlayerProject.Models;
+
+namespace Telhai.DotNet.PlayerProject.Services
+{
+    /// <summary>
+    /// service calling iTunes search API
+    /// </summary>
+    public class ItunesService
+    {
+        // init httpClient with prefix domain
+        private static readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://itunes.apple.com/")
+        };
+
+        public async Task<ItunesTrackInfo?> SearchOneAsync(
+            string songTitle,
+            CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(songTitle))
+                return null;
+
+            // build the rrequest URL
+            string encodedTerm = Uri.EscapeDataString(songTitle); // here we'll have what we are searchinf
+            string url = $"search?term={encodedTerm}&media=music&limit=1";
+
+            using HttpResponseMessage response =
+                await _httpClient.GetAsync(url, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            // get the response content as JSON string
+            string json = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            // deserialize the JSON to ItunesSearchResponse object
+            var data = JsonSerializer.Deserialize<ItunesSearchResponse>(
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+            var item = data?.Results?.FirstOrDefault();
+            if (item == null)
+                return null;
+
+            return new ItunesTrackInfo
+            {
+                TrackName = item.TrackName,
+                ArtistName = item.ArtistName,
+                AlbumName = item.CollectionName,
+                ArtworkUrl = item.ArtworkUrl100
+            };
+        }
+    }
+}
